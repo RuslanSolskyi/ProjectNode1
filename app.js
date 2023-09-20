3ebb3f4d9af1fcb3f5e80fe913622066013db11d
RuslanSolskyi <rus.solskiy@gmail.com>
2023-09-20 23:05:42 +0300

    HW2

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
-// // const {sayHallo} = require('./header')
-// // sayHallo();
-// const os = require('os');
-//
-// console.log(os.arch());
-// console.log(os.cpus());
-
-
 
 
 const fs = require('fs');
-const path = require('path');
 
+const users = [
+    { id: 1, name: 'Іван', email: 'ivan@example.com', age: 30, gender: 'чоловік' },
+    { id: 2, name: 'Марія', email: 'maria@example.com', age: 25, gender: 'жінка' },
+    { id: 3, name: 'Петро', email: 'petro@example.com', age: 35, gender: 'чоловік' },
+    { id: 4, name: 'Ольга', email: 'olga@example.com', age: 28, gender: 'жінка' },
+    { id: 5, name: 'Андрій', email: 'andriy@example.com', age: 32, gender: 'чоловік' },
+    { id: 6, name: 'Наталія', email: 'natalia@example.com', age: 27, gender: 'жінка' },
+    { id: 7, name: 'Максим', email: 'maxim@example.com', age: 29, gender: 'чоловік' },
+    { id: 8, name: 'Софія', email: 'sofia@example.com', age: 26, gender: 'жінка' },
+    { id: 9, name: 'Анна', email: 'anna@example.com', age: 31, gender: 'жінка' },
+    { id: 10, name: 'Олександр', email: 'oleksandr@example.com', age: 33, gender: 'чоловік' }
+];
 
-const mainFolder = 'myFolder';
-if (!fs.existsSync(mainFolder)) {
-    fs.mkdirSync(mainFolder);
-}
+// Перетворюємо об'єкт `users` в рядок JSON
+const usersJSON = JSON.stringify(users, null, 2);
 
+// Записуємо рядок JSON у файл db.json
+fs.writeFileSync('db.json', usersJSON);
 
-for (let i = 1; i <= 5; i++) {
-    const folderName = path.join(mainFolder, `folder${i}`);
-    const fileName = path.join(mainFolder, `file${i}.txt`);
-
-
-    if (!fs.existsSync(folderName)) {
-        fs.mkdirSync(folderName);
-        console.log(`FOLDER: ${folderName}`);
-    }
-
-    if (!fs.existsSync(fileName)) {
-        fs.writeFileSync(fileName, 'This is a sample file content.');
-        console.log(`FILE: ${fileName}`);
-    }
-}
+console.log('Дані успішно збережено у файл db.json');
